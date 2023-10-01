c3888c4ab44d226840b448cacc18815b2ca7194c
febyardhani <142764551+febyardhani@users.noreply.github.com>
2023-10-02 06:11:12 +0700

    ubah

---
+++ js/validation.js
@@ -0,0 +1,21 @@
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  var usernameError = document.getElementById("usernameError");
+  var passwordError = document.getElementById("passwordError");
+
+  usernameError.textContent = "";
+  passwordError.textContent = "";
+
+  // Simulated validation
+  if (username !== "demo" || password !== "password") {
+    passwordError.textContent = "Invalid username or password.";
+    return;
+  }
+
+  // Redirect to landing page (replace with actual landing page URL)
+  window.location.href = "beranda.html";
+});
