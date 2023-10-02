aa91f3cafa1b456bcd0a56e9450738fd65060c89
Picessa Kresna <kresna.picessa@gmail.com>
2023-10-02 22:12:13 +0800

    update

--- js/beranda.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 0);
-});
-
-function redirectToLoginPage() {
-  window.location.href = "login.html";
-}
